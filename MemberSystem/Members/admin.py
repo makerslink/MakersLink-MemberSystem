30aa32a19a55175e652c2ccddb7b1ba113ecc9c1
Atrixiex <bobo.en@gmail.com>
2022-03-16 21:43:22 +0100

    Completed custom user model, only basic change to use email instead of username

--- MemberSystem/Members/admin.py
+++ MemberSystem/Members/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import Member
 
 # Register your models here.
-admin.site.register(Member, UserAdmin)
+class MemberAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = Member
+    list_display = ('email','firstname','surname', 'is_staff', 'is_superuser', 'is_active',)
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'email')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal information', {'fields': ('firstname','surname')}),
+        ('Permissions', {'fields': ('is_staff','is_superuser', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'firstname','surname', 'is_staff', 'is_superuser', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(Member, MemberAdmin)
--- MemberSystem/Members/models.py
+++ MemberSystem/Members/models.py
@@ -1,12 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from .managers import MemberManager
 
 # Create your models here.
-class Member(AbstractUser):
+class Member(AbstractUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = ['firstname', 'surname']
 
+    objects = MemberManager()
+
+    # Remove username field
+    username = None
     email = models.EmailField(unique=True, verbose_name="E-mail")
     firstname = models.CharField(max_length=100, verbose_name="Firstname", blank=False, null=False)
     surname = models.CharField(max_length=200, verbose_name="Surname", blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Member'
+        verbose_name_plural = 'Members'
